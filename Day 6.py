4bc0c9c21a5f58013f1f8bba2d4c0218271cedc5
yash ambekar <yashamb444@gmail.com>
2022-12-12 14:20:49 +0530

    UpdaTE

---
+++ Day 6.py
@@ -0,0 +1,40 @@
+# Variables and Data Types
+
+# Everything in Python is an OBJECT
+
+# Variables are containers that store data
+a = 100
+b = "Harry" + " " + "Bhai"
+Harry = False
+c= complex(1+8j)
+
+print(c)
+print(a+10)
+print(b)
+print(Harry, b)
+
+print(type(b))
+print(type(a))
+print(type(Harry))
+
+# Built-in Data Types in Python
+
+'''
+Integer
+Float
+Complex
+Boolean
+Text
+Sequence
+List
+Dictionary (Map)
+Tuple
+'''
+
+list1 = [8, 9, [-4,-5], -5, 4]
+tuple1 = (("Parrot","Eagle"),("Lion","Tiger"))
+Dict1 = {"name":"Yash Ambekar", "Age":21, "Gender":"M"}
+
+print(list1)
+print(tuple1)
+print(Dict1)
---
+++ Ex1.py
@@ -0,0 +1,64 @@
+#  Exercise no 1 :- 
+
+# Operators in python
+'''
+    Addition        - +
+    Subtraction     - -
+    Multiplication  - *
+    Exponential     - **
+    Modulus         - %
+    Division        - /
+    Floor Division  - //
+'''
+
+# Python Calculator
+'''
+ch=0
+
+while True:
+    print("1. Addition       => a + b")
+    print("2. Subtraction    => a - b")
+    print("3. Multiplication => a * b")
+    print("4. Division       => a / b")
+    print("5. Floor Division => a // b")
+    print("6. Modulus        => a  b")
+    print("7. Exponential    => a ^ b")
+    print("8. Exit")
+
+    ch = input("Enter your choice :- ")
+
+    a = input("Enter Num 1 :- ")
+    b = input("Enter Num 2 :- ")
+    print(ch)
+
+    if ch==1:
+        print(a+b)
+    elif ch==2:
+        print(a-b)
+    elif ch==3:
+        print(a*b)
+    elif ch==4:
+        print(a/b)
+    elif ch==5:
+        print(a//b)
+    elif ch==6:
+        print(a%b)
+    elif ch==7:
+        print(a**b)
+    else:
+        break
+
+print("Successfully Done Operations!!!")
+'''
+
+a = int(input("Enter num1 :- "))
+b = int(input("Enter num2 :- "))
+
+print("Addition of ", a ," and ", b ," is : ", a+b)
+print("Subtraction of ", a ," and ", b ," is : ", a-b)
+print("Multiplication of ", a ," and ", b ," is : ", a*b)
+print("Division of ", a ," and ", b ," is : %.4f"% (a/b))
+print("Division of ", a ," and ", b ," is : {0:.3f}". format(a/b))
+print("Floor Division of ", a ," and ", b ," is : ", a//b)
+print("Exponential of ", a ," and ", b ," is : ", a**b)
+print("Modulus of ", a ," and ", b ," is : ", a%b)
